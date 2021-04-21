6333bbf928e4977f2de43c79c1803e4d52c9b2fd
Jose Carlos Herrera <jcherrera98@gmail.com>
2021-04-20 22:42:06 -0600

    agregando JWT a la mitad

--- routes/users.js
+++ routes/users.js
@@ -1,5 +1,10 @@
 var express = require("express");
+var jwt = require('jsonwebtoken');
+var bcrypt = require('bcryptjs');
+var config = require('../public/javascripts/configs')
+
 var router = express.Router();
+
 const db = require("../db");
 const dbName = "luliDB";
 const collectionName = "users";
@@ -11,25 +16,36 @@ db.initialize(dbName, collectionName, function (dbCollection) {
   });
     /* POST LOGIN. */
   router.post("/", function (req, res, next) {
+    
+
     dbCollection.findOne({ usuario: req.body.correo }, (error, user) => {
       if (error) {
         res.status(500).json({ error: "Internal Error" });
         throw error;
       }
-      if (user && user.contrasena === req.body.contrasena) {
+      var passwordIsValid = bcrypt.compareSync(req.body.contrasena, user.contrasena);
+      if (user && passwordIsValid) {
         console.log("User and password is correct");
-        res.status(201).json(user);
+        var token = jwt.sign({ id: user._id }, config.secret, {
+          expiresIn: 86400 // expires in 24 hours
+        });
+        res.status(201).json({auth: true, token});
       } else if (user == null) {
-        res.status(404).json({ error: "404 Not found" });
+        res.status(401).send({ auth: false, token: null });
       } else {
         console.log("Credentials wrong");
-        res.status(404).json({ data: "Login invalid" });
+        res.status(401).send({ auth: false, token: null });
       }
     });
   });
     /* POST REGISTER. */
   router.post("/register", function (req, res, next) {
-    let usuario = req.body
+    var hashedPassword = bcrypt.hashSync(req.body.contrasena, 8);
+    let usuario = {
+      correo: req.body.correo,
+      contrasena: hashedPassword
+    }
+    
     dbCollection.findOne({ correo: usuario.correo }, (error, user) => {
       if (error) {
         res.status(500).json({ error: "Internal Error" });
@@ -49,6 +65,31 @@ db.initialize(dbName, collectionName, function (dbCollection) {
         });
       }
     });
+  });
+
+  router.get('/me', function(req, res, next) {
+    
+    var token = req.headers['x-access-token'];
+    if (!token) return res.status(401).send({ auth: false, message: 'No token provided.' });
+    
+    jwt.verify(token, config.secret, function(err, decoded) {
+      if (err) return res.status(500).send({ auth: false, message: 'Failed to authenticate token.' });
+      
+      User.findById(decoded.id, 
+      { password: 0 }, // projection
+      function (err, user) {
+        if (err) return res.status(500).send("There was a problem finding the user.");
+        if (!user) return res.status(404).send("No user found.");
+          
+        // res.status(200).send(user); Comment this out!
+        next(user); // add this line
+      });
+    });
+  });
+  
+  // add the middleware function
+  router.use(function (user, req, res, next) {
+    res.status(200).send(user);
   });
 });
 
