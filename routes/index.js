3b8c437a31551dd55c4a8bbe65dbd5886840feb4
Jose Carlos Herrera <jcherrera98@gmail.com>
2021-04-20 21:43:21 -0600

    agregando registro y login de usuarios

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,11 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.json({ verify: 'true' });
+router.get("/", function (req, res, next) {
+  res.json({ verify: "true" });
 });
+
+
 
 module.exports = router;
--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,55 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
+const db = require("../db");
+const dbName = "luliDB";
+const collectionName = "users";
 
-/* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send({users: 'users'});
+db.initialize(dbName, collectionName, function (dbCollection) {
+    /* GET VERIFY. */
+  router.get("/", function (req, res, next) {
+    res.json({ verify: "true" });
+  });
+    /* POST LOGIN. */
+  router.post("/", function (req, res, next) {
+    dbCollection.findOne({ usuario: req.body.correo }, (error, user) => {
+      if (error) {
+        res.status(500).json({ error: "Internal Error" });
+        throw error;
+      }
+      if (user && user.contrasena === req.body.contrasena) {
+        console.log("User and password is correct");
+        res.status(201).json(user);
+      } else if (user == null) {
+        res.status(404).json({ error: "404 Not found" });
+      } else {
+        console.log("Credentials wrong");
+        res.status(404).json({ data: "Login invalid" });
+      }
+    });
+  });
+    /* POST REGISTER. */
+  router.post("/register", function (req, res, next) {
+    let usuario = req.body
+    dbCollection.findOne({ correo: usuario.correo }, (error, user) => {
+      if (error) {
+        res.status(500).json({ error: "Internal Error" });
+        throw error;
+      } else if (user && user.correo == req.body.correo){
+        res.status(201).json({ correo: user.correo });
+      } else {
+        dbCollection.insertOne(usuario, (errorInsert, result) => {
+          // callback of insertOne
+          if (errorInsert) throw errorInsert;
+          // return updated list
+          dbCollection.find().toArray((_error, _result) => {
+            // callback of find
+            if (_error) throw _error;
+            res.status(201).json(_result);
+          });
+        });
+      }
+    });
+  });
 });
 
 module.exports = router;
